9020fb638661a32d904418adcd17c30d714ba1f1
Martin Wong <martinytwong2020@gmail.com>
2022-03-08 17:12:44 -0800

    Image Slider on home page
    I changed the html layout for the home gallery to have be a slide. I added the jquery to allow you slide through the images.

--- js/app.js
+++ js/app.js
@@ -16,3 +16,43 @@ window.onscroll = function () {
     scrolled = false;
   }
 };
+var slideIndex=0;
+var imageCycle=[
+  "img/homepageImages/IMG_3290.JPG",
+  "img/homepageImages/IMG_6130.JPG",
+  "img/homepageImages/IMG_5951.JPG"
+];
+ function DisplaySlide(index){
+  $(".grid-img").attr("src",imageCycle[index]);
+  console.log("here");
+ }
+
+ DisplaySlide(slideIndex);
+
+ function SlideChange(slidenumber){
+  if(slideIndex==0&&slidenumber==-1){
+    slideIndex=2;
+  }
+  else if(slideIndex==2&&slidenumber==1){
+    slideIndex=0;
+  }
+  else{
+    if(slidenumber==1){
+      slideIndex++;
+    }
+    else{
+      slideIndex--;
+    }
+  }
+  DisplaySlide(slideIndex);
+ }
+
+function hi(){
+  alert("hi");
+}
+$(".back").click(function(){
+  SlideChange(-1);
+});
+$(".next").click(function(){
+  SlideChange(1);
+});
